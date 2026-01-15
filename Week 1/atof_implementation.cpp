5fa97482f6af0ea569d6d92aabb560ead3814ac8
StutiAgrITT <stuti.agrawal@intimetec.com>
2026-01-15 17:00:50 +0530

    Improved formatting

--- Week 1/atof_implementation.cpp
+++ Week 1/atof_implementation.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<string>
 
-double StringToDouble(std::string number){
+double StringToDouble(std::string number) {
     double result = 0.0;
     double multiplying_factor = 1;
     int index = 0;
     bool is_negative = false;
 
-    while(number[index] == ' ') index++;
+    while (number[index] == ' ') index++;
 
-    if(number[index] == '-' || number[index] == '+'){
-        if(number[index] == '-'){
+    if (number[index] == '-' || number[index] == '+') {
+        if (number[index] == '-') {
             is_negative = true;
         }
         index++;
     }
 
-    while(index < number.length() && (number[index] >= '0' && number[index] <= '9')){
+    while (index < number.length() && (number[index] >= '0' && number[index] <= '9')) {
         result = result * 10 + (number[index] - '0');
         index++;
     }
 
-    if(number[index] == '.'){
+    if (number[index] == '.') {
         index++;
-        while(index < number.length() && (number[index] >= '0' && number[index] <= '9')){
+        while (index < number.length() && (number[index] >= '0' && number[index] <= '9')) {
             multiplying_factor *= 0.1;
             result += ((number[index] - '0') * multiplying_factor);
             index++;
@@ -32,10 +32,11 @@ double StringToDouble(std::string number){
     return (is_negative) ? -result : result;
 }
 
-int main(){
+int main() {
     std::string number;
     std::cout << "Enter string: ";
     std::cin >> number;
     double number_float = StringToDouble(number);
     std::cout << "After converting to double: " << number_float << std::endl;
+    return 0;
 }
--- Week 1/butterfly_pattern.cpp
+++ Week 1/butterfly_pattern.cpp
@@ -6,9 +6,9 @@ int StringToInteger(std::string number) {
         return -1;
     }
     int number_int = 0;
-    for (int i = 0; i < number.length(); i++) {
-        if(number[i] > '9' || number[i] < '0') return -1;
-        number_int = (number_int * 10) + (number[i] - '0');
+    for (int index = 0; index < number.length(); index++) {
+        if (number[index] > '9' || number[index] < '0') return -1;
+        number_int = (number_int * 10) + (number[index] - '0');
     }
     return number_int;
 }
@@ -22,37 +22,36 @@ int GetValidInteger() {
         if(number_int != -1) return number_int;
         std::cout << "Invalid number. Enter again: ";
     }
-    return -1;
 }
 
 void ButterflyPattern(int num) {
-    for (int i = 1; i < num; i++) {
-        for (int j = 1; j <= i; j++) {
-            (j % 2) != 0 ? std::cout << "1 " : std::cout << "0 "; 
+    for (int row = 1; row < num; row++) {
+        for (int column = 1; column <= row; column++) {
+            (column % 2) != 0 ? std::cout << "1 " : std::cout << "0 "; 
         }
-        for (int space = 1; space <= (2 * num - 1) - (2 * i); space++) {
+        for (int space = 1; space <= (2 * num - 1) - (2 * row); space++) {
             std::cout << "  ";
         }
-        for (int j = 1; j <= i; j++) {
-            ((num + j) % 2) == 0 ? std::cout << "1 " : std::cout << "0 "; 
+        for (int column = 1; column <= row; column++) {
+            ((num + column) % 2) == 0 ? std::cout << "1 " : std::cout << "0 "; 
         }
         std::cout << std::endl;
     }
 
-    for (int i = 1; i < 2 * num; i++) {
-        (i % 2 == 1) ? std::cout << "1 " : std::cout << "0 ";
+    for (int row = 1; row < 2 * num; row++) {
+        (row % 2 == 1) ? std::cout << "1 " : std::cout << "0 ";
     }
     std::cout << std::endl;
 
-    for (int i = num - 1; i > 0; i--) {
-        for (int j = 1; j <= i; j++) {
-            (j % 2) != 0 ? std::cout << "1 " : std::cout << "0 ";
+    for (int row = num - 1; row > 0; row--) {
+        for (int column = 1; column <= row; column++) {
+            (column % 2) != 0 ? std::cout << "1 " : std::cout << "0 ";
         }
-        for (int space = 1; space <= (2 * num - 1) - (2 * i); space++) {
+        for (int space = 1; space <= (2 * num - 1) - (2 * row); space++) {
             std::cout << "  ";
         }
-        for (int j = 1; j <= i; j++) {
-            ((num + j) % 2) == 0 ? std::cout << "1 " : std::cout << "0 "; 
+        for (int column = 1; column <= row; column++) {
+            ((num + column) % 2) == 0 ? std::cout << "1 " : std::cout << "0 "; 
         }
         std::cout << std::endl;
     }
@@ -61,4 +60,5 @@ void ButterflyPattern(int num) {
 int main() {
     int number_of_rows = GetValidInteger();
     ButterflyPattern(number_of_rows);
+    return 0;
 }
